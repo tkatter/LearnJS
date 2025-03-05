0c7e848758446f30a8895bb14929e09acb486a0b
tkatter <tjkatter@gmail.com>
2025-03-04 19:39:11 -0600

    went over variable naming conventions.

--- Udemy-Course/01-Fundamentals-Part-1/starter/script.js
+++ Udemy-Course/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,44 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+// Value = smallest unit of information in JavaScript
+
+// console.log("Jonas");    "Jonas" is a value
+// console.log(23);         23 is a value
+
+// What are variables??
+// A box into which we can store a value.
+
+// Declaring a variable
+let firstName = "Thomas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Naming Variable rules:
+// camelCase
+let camel = 'one word with camelCase.';
+let camelCase = 'two words with camelCase.';
+let camelCaseNaming = 'three words with camelCase.';
+
+// Can not start variable name with number
+let 3years = 3;
+
+// Can only contain numbers, letters, underscores, and dollar signs
+// Can not name variables with reserved JavaScript keywords i.e. new, function
+// "name" is reserved, so don't call variables only "name"
+// Start variables in lowercase
+let firstVar = 1;
+
+// Variables in all uppercase are constants
+let PI = 3.1415;
+
+// Make variable names descriptive of their values -- for cleaner code
+let myFirstJob = 'programmer';
+let mySecondJob = 'teacher';
+// versus:
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
