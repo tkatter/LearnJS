a9b1c042273281afcaf9e153c803003dec0d758d
tkatter <tjkatter@gmail.com>
2025-03-13 08:02:37 -0500

    finished PROJECT #2: MODAL WINDOW

--- Udemy-Course/06-Modal/starter/script.js
+++ Udemy-Course/06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+// need to select the modal, overlay, close-modal, and show-modal
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
