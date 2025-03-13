ed8f18c3ab1e5851ea89eaadb91363d46ba579bb
tkatter <tjkatter@gmail.com>
2025-03-13 06:59:29 -0500

    finished PROJECT #1: GUESS MY NUMBER

--- Udemy-Course/05-Guess-My-Number/starter/script.js
+++ Udemy-Course/05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 // WHAT IS DOM?
 // Documentation Object Model:
@@ -14,3 +14,66 @@ document.querySelector('.number').textContent = '37';
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+const lostGame = function () {
+  document.querySelector('.number').textContent = secretNumber;
+  displayMessage('You lost the game!');
+  document.querySelector('.score').textContent = 0;
+  // Change CSS styling
+  document.querySelector('body').style.backgroundColor = '#ff4343'; // changes background color
+  document.querySelector('.number').style.width = '30rem'; // changes number box width
+};
+const wonGame = function () {
+  document.querySelector('.number').textContent = secretNumber;
+  displayMessage('Correct number!');
+  // Change CSS styling
+  document.querySelector('body').style.backgroundColor = '#60b347'; // changes background color
+  document.querySelector('.number').style.width = '30rem'; // changes number box width
+};
+
+// "Check" button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    wonGame();
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+    // When the guess is not correct
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      lostGame();
+    }
+  }
+});
+
+// "Again" button
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222'; // changes background color
+  document.querySelector('.number').style.width = '15rem'; // changes number box width
+});
