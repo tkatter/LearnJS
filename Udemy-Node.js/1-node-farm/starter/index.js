739bee069261f5cc9ac4c1595655a9fb91526a8a
tkatter <tjkatter@gmail.com>
2025-03-13 21:59:39 -0500

    continued

--- Udemy-Node.js/1-node-farm/starter/index.js
+++ Udemy-Node.js/1-node-farm/starter/index.js
@@ -1,3 +1,23 @@
-const fs = require("fs");
+const fs = require('fs');
+const http = require('http');
+/*
+// reading and writing to and from files
+const textIn = fs.readFileSync("./txt/input.txt", "utf8");
+const textOut = `This is what we know about the avocado: ${textIn}\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written!");
+*/
 
-fs.readFileSync();
+const server = http.createServer((req, res) => {
+  let reqInfo = req;
+  console.log(reqInfo);
+  // fs.writeFile('./txt/req.txt', `${reqInfo}`, 'utf-8', err => {
+  //   if (err) throw err;
+  //   console.log('file written');
+  // });
+  res.end('Hey you ;)');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000...');
+});
