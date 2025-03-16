245c8bb6b69295f64bab980694e00a9f54df55c8
tkatter <tjkatter@gmail.com>
2025-03-15 17:39:59 -0700

    working through replacing template placeholders

--- Udemy-Node.js/1-node-farm/starter/index.js
+++ Udemy-Node.js/1-node-farm/starter/index.js
@@ -8,8 +8,37 @@ const textOut = `This is what we know about the avocado: ${textIn}\nCreated on $
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written!");
 */
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOTORGANIC%}/g, 'not-organic');
+  }
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/TEMPLATE-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/TEMPLATE-product.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/TEMPLATE-card.html`,
+  'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   // let reqInfo = req;
   // console.log(reqInfo);
@@ -20,7 +49,11 @@ const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW page.');
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT page.');
   } else if (pathName === '/api') {
@@ -37,4 +70,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000...');
+  console.log(dataObj[0].productName);
 });
