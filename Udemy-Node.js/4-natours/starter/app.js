b5c40cdcfd79bd6de8b6efffd38b8b5d1cbaa80e
tkatter <tjkatter@gmail.com>
2025-03-20 11:01:04 -0700

    finished SETTING UP EXPRESS AND BASIC ROUTING lecture

--- Udemy-Node.js/4-natours/starter/app.js
+++ Udemy-Node.js/4-natours/starter/app.js
@@ -6,11 +6,18 @@ const app = express();
 
 // Route
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side!');
+  // res.status(200).send('Hello from the server side!');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.status(200).send('You can post to this endpoint...');
 });
 
 // Server
-const port = 8000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
