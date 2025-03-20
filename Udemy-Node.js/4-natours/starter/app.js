fd0649247ae8c878d05ba95ddc8c3c9694a1f976
tkatter <tjkatter@gmail.com>
2025-03-20 10:51:35 -0700

    started the express server. found that REQUIRES EXPRESS@4.16.4

--- Udemy-Node.js/4-natours/starter/app.js
+++ Udemy-Node.js/4-natours/starter/app.js
@@ -1,1 +1,16 @@
-const express = require'(express)';
+// Importing modules
+const express = require('express');
+
+// Core Variables
+const app = express();
+
+// Route
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from the server side!');
+});
+
+// Server
+const port = 8000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
