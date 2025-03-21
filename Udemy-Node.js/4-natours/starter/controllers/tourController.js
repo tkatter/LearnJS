6b2bc9959621d611631183134f3c4fca1ecbb2dd
tkatter <tjkatter@gmail.com>
2025-03-21 01:10:55 -0500

    refractored checking id url parameter for validity

--- Udemy-Node.js/4-natours/starter/controllers/tourController.js
+++ Udemy-Node.js/4-natours/starter/controllers/tourController.js
@@ -5,6 +5,15 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length - 1) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,18 +25,8 @@ exports.getAllTours = (req, res) => {
   });
 };
 exports.getTour = (req, res) => {
-  // console.log(req.params);
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
-
-  // Check if tour is undefined (tour doesn't exist)
-  if (!tour) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -36,14 +35,10 @@ exports.getTour = (req, res) => {
   });
 };
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-
   // Make a new ID
   const newId = tours[tours.length - 1].id + 1;
-
   // Replace the request ID with the new ID
   const newTour = Object.assign({ id: newId }, req.body);
-
   // Push the new tour to the original tours array
   tours.push(newTour);
   // Write this new tour addition to the original data file
@@ -59,13 +54,6 @@ exports.createTour = (req, res) => {
   );
 };
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length - 1) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -74,13 +62,6 @@ exports.updateTour = (req, res) => {
   });
 };
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length - 1) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
