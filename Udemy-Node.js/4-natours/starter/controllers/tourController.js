073f41e508259e7c47070a1f44f3e647f74d90b0
tkatter <tjkatter@gmail.com>
2025-03-21 06:28:59 -0500

    re-wrote apis for use with mongoDB (CRU), yet to do Delete

--- Udemy-Node.js/4-natours/starter/controllers/tourController.js
+++ Udemy-Node.js/4-natours/starter/controllers/tourController.js
@@ -1,10 +1,93 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
 // read tours-simple data file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
+
+/* Check ID Middleware
 exports.checkID = (req, res, next, val) => {
   if (req.params.id * 1 > tours.length - 1) {
     return res.status(404).json({
@@ -14,7 +97,9 @@ exports.checkID = (req, res, next, val) => {
   }
   next();
 };
+*/
 
+/* Check Body Middleware
 exports.checkBody = (req, res, next) => {
   if (
     !req.body.name ||
@@ -29,29 +114,9 @@ exports.checkBody = (req, res, next) => {
   }
   next();
 };
+*/
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
+/* Original method
 exports.createTour = (req, res) => {
   // Make a new ID
   const newId = tours[tours.length - 1].id + 1;
@@ -71,19 +136,4 @@ exports.createTour = (req, res) => {
     }
   );
 };
-
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here>',
-    },
-  });
-};
-
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
+*/
--- Udemy-Node.js/4-natours/starter/server.js
+++ Udemy-Node.js/4-natours/starter/server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const database = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Connect to MongoDB database
+mongoose.connect(database).then(() => {
+  console.log('DB connection successful!');
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
