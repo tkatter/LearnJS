f3941c2afa09fb844b8a9d388b10c6065a9bd072
tkatter <tjkatter@gmail.com>
2025-03-22 12:03:00 -0500

    made global error middleware and handler

--- Udemy-Node.js/4-natours/starter/app.js
+++ Udemy-Node.js/4-natours/starter/app.js
@@ -11,12 +11,13 @@ curl -H 'Content-Type: application/json' \
 http://127.0.0.1:3000/api/v1/tours
 */
 
-// Importing modules
 // CORE MODULES
 const express = require('express');
 const morgan = require('morgan');
 
 // USER MODULES
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -46,11 +47,30 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // MIDDLEWARE AFTER REQUEST WAS NOT REACHED BY OUR ROUTERS
+/* OG simple error middleware for uncaught url
 app.all('*', (req, res, next) => {
   res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl}`,
   });
+  next();
 });
+*/
+
+/* v2 of error middleware
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl}`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+*/
+
+// final error middleware using class in ./utils/appError.js
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
