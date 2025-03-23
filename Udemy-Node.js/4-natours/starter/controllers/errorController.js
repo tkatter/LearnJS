72df01d82ba9f5ed867edf1738d7c8cff409171b
tkatter <tjkatter@gmail.com>
2025-03-23 00:36:00 -0500

    added error handler for invalid id query

--- Udemy-Node.js/4-natours/starter/controllers/errorController.js
+++ Udemy-Node.js/4-natours/starter/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -17,7 +24,7 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     // 1) Log error
-    console.error('ERROR', err);
+    // console.error('ERROR', err);
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
@@ -33,6 +40,8 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = err;
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
