a47a4029ee3c4ed8e7da6b5a94035b03a69fb22f
tkatter <tjkatter@gmail.com>
2025-03-26 11:55:11 -0500

    worked with destructuring arrays and objects

--- Udemy-Course/09-Data-Structures-Operators/starter/script.js
+++ Udemy-Course/09-Data-Structures-Operators/starter/script.js
@@ -44,4 +44,69 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+// Destructuring Objects
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Setting Default Values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating Variables
+let a = 111;
+let b = 999; // a = 999, b = 111
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+/*
+// Manual Destructuring Array
+const arr = [7, 8, 9];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+
+// Destructuring Array
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+// Setting Default Values
+const [d = 1, e = 1, f = 1] = [6, 9];
+console.log(d, e, f);
+
+// Skipping Values in Destructuring
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// Switching Variables
+// Manually
+const temp = main;
+main = secondary;
+secondary = temp;
+
+// With Destructuring
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(`${starter}, ${mainCourse}`);
+
+// Nested Destructuring
+const nested = [2, 4, [5, 6]];
+
+// Return the entire array in element 2 of nested array
+const [i, , j] = nested;
+console.log(i, j);
+
+// Return the individual elements in element 2 of nested array
+const [l, , [m, n]] = nested;
+console.log(l, m, n);
+*/
